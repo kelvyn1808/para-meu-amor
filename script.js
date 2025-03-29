f8503927704aed30856a18006108e7a698346351
kelvyn1808 <kelvyn.sssouza18@gmail.com>
2025-03-29 07:42:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão "Surpresa"
+const surpriseButton = document.querySelector("button");
+
+// Adiciona um evento de clique no botão
+surpriseButton.addEventListener("click", function() {
+    alert("Você é a pessoa mais especial para mim. Te amo!❤️❤️❤️");
+});
